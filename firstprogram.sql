f2dcc84eb46a0f21b4c00dad511b69ca37219ad2
Annmaria Thankachan <165566292+AnnmariaThankachan76@users.noreply.github.com>
2025-02-20 05:53:35 +0530

    Update firstprogram.sql

--- firstprogram.sql
+++ firstprogram.sql
@@ -3,3 +3,16 @@ id int primary key,
 name vrchar(50),
 age int not null,
 );
+
+INSERT INTO student values(1,"Tara",24);
+  INSERT INTO student values(2,"Mona",20);
+  INSERT INTO student values(3,"Priya",25);
+  INSERT INTO student values(4,"Aman",22);
+
+Select * from student;
+
+Insert into student (id,name,age) 
+ values 
+ (5,"Ann",19),
+ (6,"Sarath",23)
+
